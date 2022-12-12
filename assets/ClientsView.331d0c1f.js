cae86a4f8d4023e684c57ceb2170b0f956db47e4
Mansurkhoja <mansur.atm@gmail.com>
2022-12-12 20:48:33 +0500

    fc

--- assets/ClientsView.331d0c1f.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./p_plugin-vue_export-helper.cdc0426e.js";const t={};function c(n,r){return" clients "}const _=e(t,[["render",c]]);export{_ as default};
--- assets/ReportsView.5b2c48dd.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./p_plugin-vue_export-helper.cdc0426e.js";const e={};function t(c,o){return" reports "}const _=r(e,[["render",t]]);export{_ as default};
