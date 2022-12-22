012d4bb107d3a7c6fa01ef115acbb49aea1bb1b5
Mansurkhoja <mansur.atm@gmail.com>
2022-12-22 19:10:34 +0500

    fc

--- assets/AdminsView.e6645540.js
+++ assets/AdminsView.e6645540.js
@@ -1,1 +1,1 @@
-import{_ as e}from"./p_plugin-vue_export-helper.cdc0426e.js";const n={};function r(c,s){return" admins "}const _=e(n,[["render",r]]);export{_ as default};
+import{_ as e}from"./pplugin-vue_export-helper.cdc0426e.js";const n={};function r(c,s){return" admins "}const _=e(n,[["render",r]]);export{_ as default};
--- assets/PricesView.529567f4.js
+++ assets/PricesView.529567f4.js
@@ -1,1 +1,1 @@
-import{_ as e}from"./p_plugin-vue_export-helper.cdc0426e.js";const r={};function c(s,t){return" prices "}const n=e(r,[["render",c]]);export{n as default};
+import{_ as e}from"./pplugin-vue_export-helper.cdc0426e.js";const r={};function c(s,t){return" prices "}const n=e(r,[["render",c]]);export{n as default};
