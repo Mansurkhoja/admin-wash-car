8e6c60e7e258e1c41b3d5ef9c2de92ce31fcca76
Mansurkhoja <mansur.atm@gmail.com>
2022-12-29 20:42:55 +0500

    fc

--- assets/WashersView.70eaa801.js
+++ assets/WashersView.70eaa801.js
@@ -1,1 +1,1 @@
-import{_ as e}from"./pplugin-vue_export-helper.cdc0426e.js";const r={};function s(c,t){return" washers "}const n=e(r,[["render",s]]);export{n as default};
+import{_ as e}from"./p_plugin-vue_export-helper.cdc0426e.js";const r={};function s(c,t){return" washers "}const n=e(r,[["render",s]]);export{n as default};
